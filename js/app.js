1f786cfac38b25cecf2062c2977d84bf7b738237
Josh Eimer <jeimer@Joshs-MacBook-Pro.local>
2016-09-20 15:57:18 -0500

    Design and for validation completed

--- js/app.js
+++ js/app.js
@@ -14,13 +14,13 @@ $(function(){
 
 
 	// a listener to add contact info to array
-	$('button.add-contact').click(function(){
+	$('form').submit(function(){
+		// validation();
 		var firstName = $('input#first-name').val();
 		var lastName = $('input#last-name').val();
 		var phones = getPhones();
 		var addresses = getAddresses();
 
-		console.log(addresses)
 		var currentContact = Object.create(Contact);
 		currentContact.firstName = firstName;
 		currentContact.lastName = lastName;
@@ -30,11 +30,17 @@ $(function(){
 		contactsArray.push(currentContact);
 		console.log(contactsArray);
 		$('input').val('');
+
 		//Display contactsArray under Contacts heading
-		var temp = contactsArray[contactsArray.length - 1]; 
-		// id
-		// data-attributes (html5) 
-		$('ul.contacts-list').append('<li><a href="#" id="' + (contactsArray.length-1) + '">' + temp.firstName + ' ' +  temp.lastName + '</a></li>');
+		
+		resetForm(contactsArray[contactsArray.length - 1], contactsArray.length-1, currentContact)
+
+		return false;
+	});
+
+	var resetForm = function(temp, contactsArrayLength, currentContact){
+		$('input').val('');
+		$('ul.contacts-list').append('<li><a href="#" id="' + (contactsArrayLength) + '">' + temp.firstName + ' ' +  temp.lastName + '</a></li>');
 		if (currentContact.address.length > 1) {
 			$('.add-phone').show();
 			$('.phone-group-1').remove();
@@ -43,10 +49,16 @@ $(function(){
 			$('.add-address').show();
 			$('.address-group-1').remove();
 		}
-	});
+	}
+
+	var validateForm = function(){
+		validateInput('.first-name');
+		validateInput('.last-name');
+
+	}
 
 	var getPhones = function(){
-		var storeLength = $('div.phone-number-group').children().length;
+		var storeLength = $('fieldset.phone-number-group').children().length;
 		var phoneList = [];
 		for (var i = 0; i < storeLength; i++) {
 			phoneList.push($('#phone-number-' + i).val());
@@ -56,14 +68,15 @@ $(function(){
 
 
 	var getAddresses = function(){
-		var storeLength = $('div.address-fields').children().length;
+		var storeLength = $('fieldset.address-fields').children().length;
 		var addressList = [];
 		for(var i = 0; i < storeLength; i++) {
 			var street = $('#street-' + i).val();
 			var city = $('#city-' + i).val();
 			var state = $('#state-' + i).val();
-
-			addressList.push(street + ', ' + city + ', ' + state);
+			if(street || city || state !== '') {
+				addressList.push(street + ', ' + city + ', ' + state);
+			}
 		}
 		return addressList
 	}
@@ -76,24 +89,37 @@ $(function(){
 		$('section.contact-info').children().remove();
 		$('section.contact-info').append('<h3></h3><p>First Name: <span class="first-name"></span></p><p>Last Name: <span class="last-name"></span></p><p>Phone Numbers</p><ul class="phone-list"></ul><p>Addresses</p><ul class="address-list"></ul>')
 		$('section.contact-info').show();
+		$('section.contact-info h3').text(selectedContact.firstName + ' ' + selectedContact.lastName)
 		$('.first-name').text(selectedContact.firstName);
 		$('.last-name').text(selectedContact.lastName);
 		for(var i = 0; i < selectedContact.phones.length; i++) {
 			$('.phone-list').append('<li>' + selectedContact.phones[i] + '</li>');
 		}
-		for(var j = 0; j < selectedContact.address.length; j++) {
+		if(selectedContact.address.length == 0){
+			$('section.contact-info p:contains(Address)').hide()
+		} else {
+			for(var j = 0; j < selectedContact.address.length; j++) {
 			$('.address-list').append('<li>' + selectedContact.address[j] + '</li>');
+			}
 		}
 	})
 
 	$('.add-phone').click(function(){
+		event.preventDefault();
 		$('.add-phone').hide()
-		$('.phone-number-group').append('<div class="phone-group-1"><label for="phone-number-1">Phone Number #2</label><input type="text" id="phone-number-1" size="50"></div>')
+		$('.phone-number-group').append('<div class="phone-group-1"><label for="phone-number-1">Phone Number #2</label><input type="text" id="phone-number-1" maxlength="11"></div>')
 	})
 
 	$('.add-address').click(function(){
+		event.preventDefault();
 		$('.add-address').hide();
-		$('.address-fields').append('<div class="address-group-1"><label for="street-1">Street #2</label><input type="text" id="street-1" size="50"><label for="city-1">City #2</label><input type="text" id="city-1" size="50"><label for="state-1">State #2</label><input type="text" id="state-1" size="50"></div>');
+		$('.address-fields').append('<div class="address-group-1"><label for="street-1">Street #2</label><input type="text" id="street-1"><label for="city-1">City #2</label><input type="text" id="city-1"><label for="state-1">State #2</label><input type="text" id="state-1"></div>');
+	})
+
+	//only numbers in telephone field
+	$('.phone-number-group').on('input', 'input[id*="phone-number"]', function(){
+		this.value = this.value.replace(/[^0-9.]/g, ''); 
+		this.value = this.value.replace(/(\..*)\./g, '$1');
 	})
 
 
